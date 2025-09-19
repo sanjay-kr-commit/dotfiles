ce2b91dd84e24e47500002b14dc674c56ee40b3a
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2025-09-19 09:45:48 +0530

    add script to detect activate venv

--- dotfiles/scripts/apply.sh
+++ dotfiles/scripts/apply.sh
@@ -1,12 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
-local SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+local SCRIPTPATH="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
 
 source "$SCRIPTPATH/debug"
 
-if [[ "$1" == "log" ]] ; then
+if [[ "$1" == "log" ]]; then
   constructDebugger
-fi 
+fi
 
 # commment below line to stop sudo override
 eval "source $SCRIPTPATH/runUserBinariesWithSudo/script"
@@ -17,12 +20,14 @@ eval "source $SCRIPTPATH/gitPass/script"
 # comment below line to disable flatpakAliases
 # eval "source $SCRIPTPATH/flatpakAlias/script"
 
-# comment below line to remove syncTheme 
+# comment below line to remove syncTheme
 eval "source $SCRIPTPATH/syncThemeToRoot/script"
 
-# comment below line to remove saveAlias and deleteAlias function 
+# comment below line to remove saveAlias and deleteAlias function
 eval "source $SCRIPTPATH/saveAlias/script"
 
-if [[ "$2" != "persist" ]] ; then
-  destructDebugger 
+eval "source $SCRIPTPATH/activateVenv/script"
+
+if [[ "$2" != "persist" ]]; then
+  destructDebugger
 fi
