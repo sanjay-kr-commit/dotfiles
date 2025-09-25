3851f9d00879200452b89c94a4c638ccc81c4ce1
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2025-09-26 00:07:47 +0530

    detect gradle builds

--- .config/nvim/lua/bash/kotlinc.sh
+++ .config/nvim/lua/bash/kotlinc.sh
@@ -1,33 +1,64 @@
 #!/bin/bash
 
-kotlinc $2 -include-runtime -d /dev/shm/main.jar &
-pid=$!
+runFile() {
+  kotlinc $2 -include-runtime -d /dev/shm/main.jar &
+  pid=$!
 
-sign="-"
+  sign="-"
 
-while [[ -d "/proc/$pid" ]]; do
-  echo -ne "\r$sign compiling code"
-  case "$sign" in
-  "|")
-    sign="/"
-    ;;
-  "/")
-    sign="-"
-    ;;
-  "-")
-    sign="\\"
-    ;;
-  "\\")
-    sign="|"
-    ;;
-  esac
+  while [[ -d "/proc/$pid" ]]; do
+    echo -ne "\r$sign compiling code"
+    case "$sign" in
+    "|")
+      sign="/"
+      ;;
+    "/")
+      sign="-"
+      ;;
+    "-")
+      sign="\\"
+      ;;
+    "\\")
+      sign="|"
+      ;;
+    esac
 
-  sleep 0.1
-done
+    sleep 0.1
+  done
 
-echo -ne "\rcompilation complete        \n"
+  echo -ne "\rcompilation complete        \n"
 
-cd /dev/shm/
-kotlin main.jar
-cd $1
-rm /dev/shm/main.jar
+  cd /dev/shm/
+  kotlin main.jar
+  cd $1
+  rm /dev/shm/main.jar
+
+}
+
+isGradleBased="$(echo "$1" | grep -o "src/\(main\|test\)/kotlin")"
+
+if [[ -z $isGradleBased ]]; then
+  runFile $1 $2
+  exit
+fi
+
+arg="$(echo $isGradleBased | grep -o "\(test\|main\)")"
+arg="$(echo $arg | sed -s 's/main/run/g')"
+
+projRootPath="$(echo "$1" | sed "s|$isGradleBased.*||")"
+
+if [[ ! -z "$(ls $projRootPath | grep -o ".*gradle.*")" ]]; then
+  echo "gradle $arg"
+  gradle $arg
+  exit
+fi
+
+projRootPath="$(echo "${projRootPath%/}" | sed 's:/[^/]*$::')"
+
+if [[ ! -z "$(ls $projRootPath | grep -o ".*gradle.*")" ]]; then
+  echo "gradle $arg"
+  gradle $arg
+  exit
+fi
+
+runFile $1 $2
